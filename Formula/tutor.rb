0bc625f0eb3b25a6c2a076f332a64a2e42c12861
tomtdhzz <tomtdhzz@gmail.com>
2026-09-07 20:19:20 +0800

    tutor 0.1.0

---
+++ Formula/tutor.rb
@@ -0,0 +1,73 @@
+class Tutor < Formula
+  desc "Local omp tutor: per-window work board, auto-mined study loop, and a daily briefing (reads omp sessions, brains via omp -p)"
+  homepage "https://github.com/tomtdhzz/tutor"
+  version "0.1.0"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/tomtdhzz/tutor/releases/download/v0.1.0/tutor-aarch64-apple-darwin.tar.xz"
+      sha256 "8828d1e45b2eab5ba21e7a16645fc11ef62ff65d954b46bacaaa0981c8fb02a1"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/tomtdhzz/tutor/releases/download/v0.1.0/tutor-x86_64-apple-darwin.tar.xz"
+      sha256 "c3e4009dd2687b5f63c3a563f6ac01166a9c29bd252f3bbaf2f5cce1987a9428"
+    end
+  end
+  if OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/tomtdhzz/tutor/releases/download/v0.1.0/tutor-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "ce1117953ce184d762fa2e1e82fb7c89915f4d22c7b16797128818bf464c362c"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/tomtdhzz/tutor/releases/download/v0.1.0/tutor-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "6357327bfe42cb903eabb934852d54bfcfa53be82e8b7a2ac177291990d7f7c6"
+    end
+  end
+  license "MIT"
+
+  BINARY_ALIASES = {
+    "aarch64-apple-darwin": {},
+    "aarch64-unknown-linux-gnu": {},
+    "x86_64-apple-darwin": {},
+    "x86_64-unknown-linux-gnu": {}
+  }
+
+  def target_triple
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+
+    "#{cpu}-#{os}"
+  end
+
+  def install_binary_aliases!
+    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+      dests.each do |dest|
+        bin.install_symlink bin/source.to_s => dest
+      end
+    end
+  end
+
+  def install
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "tutor"
+    end
+    if OS.mac? && Hardware::CPU.intel?
+      bin.install "tutor"
+    end
+    if OS.linux? && Hardware::CPU.arm?
+      bin.install "tutor"
+    end
+    if OS.linux? && Hardware::CPU.intel?
+      bin.install "tutor"
+    end
+
+    install_binary_aliases!
+
+    # Homebrew will automatically install these, so we don't need to do that
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files
+
+    # Install any leftover files in pkgshare; these are probably config or
+    # sample files.
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  end
+end
